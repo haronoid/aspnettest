e4d4f9ae63173e6759fefca8610cb26d87ce6406
haronoid <haronoid@gmail.com>
2017-05-30 17:55:48 +0900

    first

---
+++ api.Tests/Controllers/ValuesControllerTest.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using System;
+using System.Web.Http;
+using api;
+using api.Controllers;
+
+namespace api.Tests.Controllers
+{
+    [TestFixture]
+    public class ValuesControllerTest
+    {
+		[Test]
+		public void GetValue()
+        {
+            var controller = new ValuesController();
+
+            var result = controller.GetValue();
+
+            Assert.AreEqual("value", result);
+        }
+    }
+}
---
+++ api/Controllers/ValuesController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Web.Http;
+
+namespace api.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        public ValuesController()
+        {
+        }
+
+        public string GetValue()
+        {
+            return "value";
+        }
+    }
+}
